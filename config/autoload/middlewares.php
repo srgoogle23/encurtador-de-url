a75d82ae21ab69dcabbcdaaf9fc1a2605d207efe
Leonardo Oliveira <62403037+srgoogle23@users.noreply.github.com>
2024-12-28 16:17:27 -0300

    added validation

--- config/autoload/middlewares.php
+++ config/autoload/middlewares.php
@@ -9,7 +9,18 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+use Hyperf\Validation\Middleware\ValidationMiddleware;
+
+/*
+ * This file is part of Hyperf.
+ *
+ * @see     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
 return [
     'http' => [
+        ValidationMiddleware::class,
     ],
 ];
