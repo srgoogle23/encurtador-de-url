e381d142d39df8b8e5896b15893d86f2f949a85e
Leonardo Oliveira <62403037+srgoogle23@users.noreply.github.com>
2024-12-28 15:33:44 -0300

    add users routes

--- config/routes.php
+++ config/routes.php
@@ -9,9 +9,17 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+use App\Controller\UserController;
 use Hyperf\HttpServer\Router\Router;
 
 Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@index');
+
+Router::addGroup('/users', function () {
+    Router::get('', [UserController::class, 'index']);
+    Router::get('/{id}', [UserController::class, 'show']);
+    Router::post('', [UserController::class, 'store']);
+    Router::delete('/{id}', [UserController::class, 'delete']);
+});
 
 Router::get('/favicon.ico', function () {
     return '';
