f3e5023db15f5036326a0ff89ac7f391237ab960
Leonardo Oliveira <62403037+srgoogle23@users.noreply.github.com>
2025-01-03 00:58:37 -0300

    Auth for api (#6)
    * added auth endpoint
    
    * added jwt aut

--- config/routes.php
+++ config/routes.php
@@ -9,23 +9,27 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+use App\Controller\AuthController;
 use App\Controller\LinkController;
 use App\Controller\UserController;
 use Hyperf\HttpServer\Router\Router;
-
-Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@index');
+use Phper666\JWTAuth\Middleware\JWTAuthMiddleware;
 
 Router::addGroup('/users', function () {
     Router::get('', [UserController::class, 'index']);
     Router::get('/{id}', [UserController::class, 'show']);
     Router::post('', [UserController::class, 'store']);
     Router::delete('/{id}', [UserController::class, 'delete']);
-});
+}, ['middleware' => [JWTAuthMiddleware::class]]);
 
 Router::addGroup('/links', function () {
     Router::get('/byUser/{user}', [LinkController::class, 'index']);
     Router::get('/{shortenedLink}', [LinkController::class, 'show']);
     Router::post('', [LinkController::class, 'store']);
+}, ['middleware' => [JWTAuthMiddleware::class]]);
+
+Router::addGroup('/auth', function () {
+    Router::post('', [AuthController::class, 'auth']);
 });
 
 Router::get('/favicon.ico', function () {
